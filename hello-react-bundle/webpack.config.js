2e6c5e5df1931cb34962e6f99efc69d0a48cf532
Yung-Feng Chou <yungfeng73@hotmail.com>
2016-10-18 23:43:46 -0700

    suport jsx in the webpack bundle

--- hello-react-bundle/webpack.config.js
+++ hello-react-bundle/webpack.config.js
@@ -2,12 +2,24 @@
  * Created by r626884 on 10/18/2016.
  */
 module.exports = {
-    entry: './public/app.js',
+    entry: './public/app.jsx',
     output: {
         path: __dirname,
         filename: './public/bundle.js'
     },
     resolve: {
         extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                },
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/
+            }
+        ]
     }
-}
+};
