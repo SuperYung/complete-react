b40e1812e2ff73f62e1e87eb29535aa2ab9fd5a2
Yung-Feng Chou <Yung-Feng.Chou@cambiahealth.com>
2016-10-28 18:52:07 -0700

    add multiple pages

--- ReactBoilerplate/app/app.jsx
+++ ReactBoilerplate/app/app.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 ReactDOM.render(
-  <h1>Boilerplate app!</h1>,
-  document.getElementById('app')
+    <h1>Boilerplate app!</h1>,
+    document.getElementById('app')
 );
+
--- ReactWeather/app/components/Main.jsx
+++ ReactWeather/app/components/Main.jsx
@@ -2,14 +2,15 @@ var React = require('react');
 var Nav = require('Nav');
 
 var Main = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <Nav/>
-        <h2>Main Component</h2>
-      </div>
-    );
-  }
+    render: function () {
+        return (
+            <div>
+                <Nav />
+                <h2>Main Component</h2>
+                {this.props.children}
+            </div>
+        )
+    }
 });
 
 module.exports = Main;
